4d311a248230fb8d6c776e0a46e57446befac2c1
bamjam101 <herejamil123@gmail.com>
2023-06-10 12:49:41 +0530

    debugged update frontend script, it's working now.

--- deploy/02-update-frontend.js
+++ deploy/02-update-frontend.js
@@ -2,33 +2,33 @@ const { ethers, network } = require("hardhat")
 const fs = require("fs")
 
 const FRONTEND_ADDRESSES_FILE =
-  "../nextjs-raffle-dapp/constants/contractAdressses.json"
-const FRONTEND_ABI_FILE = "../nextjs-raffle-dapp/constants/abi.json"
+  "C:/Users/herej/OneDrive/Desktop/Web3/nextjs-raffle-dapp/constants/contractAddresses.json"
+const FRONTEND_ABI_FILE =
+  "C:/Users/herej/OneDrive/Desktop/Web3/nextjs-raffle-dapp/constants/abi.json"
 
 module.exports = async () => {
   if (process.env.UPDATE_FRONTEND) {
     console.log("Updating frontend...")
-    await updateContractAddresses()
-    await updateABIFile()
+    updateContractAddresses()
+    updateABIFile()
   }
 }
 
 async function updateContractAddresses() {
   const raffle = await ethers.getContract("Raffle")
   const chainId = network.config.chainId.toString()
-  const currentAddresses = JSON.parse(
+  const contractAddresses = JSON.parse(
     fs.readFileSync(FRONTEND_ADDRESSES_FILE),
     "utf8"
   )
-  if (chainId in contractAddress) {
-    if (!contractAddress[chainId].includes(raffle.address)) {
-      currentAddresses[chainId].push(raffle.address)
+  if (chainId in contractAddresses) {
+    if (!contractAddresses[chainId].includes(raffle.address)) {
+      contractAddresses[chainId].push(raffle.address)
     }
+  } else {
+    contractAddresses[chainId] = [raffle.address]
   }
-  {
-    currentAddresses[chainId] = [raffle.address]
-  }
-  fs.writeFileSync(FRONTEND_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+  fs.writeFileSync(FRONTEND_ADDRESSES_FILE, JSON.stringify(contractAddresses))
 }
 
 async function updateABIFile() {
